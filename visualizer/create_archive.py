7ee319888a49be397a703ceeeb5bd1b0f148460d
Andrey <Cjkjvfnby@gmail.com>
2022-09-15 01:46:52 +0300

    Use proper type hints

--- visualizer/create_archive.py
+++ visualizer/create_archive.py
@@ -1,8 +1,9 @@
 import io
+from typing import BinaryIO
 from zipfile import ZipFile
 
 
-def make_zip(file_names: list[str]) -> tuple[io.BytesIO, int]:
+def make_zip(file_names: list[str]) -> tuple[BinaryIO, int]:
     file = io.BytesIO()
 
     with ZipFile(file, "w") as zip_obj:
