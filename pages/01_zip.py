b6658bbd8d3c9a652234fc53e65224d7c20d1de8
Andrey <Cjkjvfnby@gmail.com>
2022-09-17 11:51:35 +0300

    Add debug for streamlit cloud

--- pages/01_zip.py
+++ pages/01_zip.py
@@ -50,3 +50,11 @@ class ExtractZip(ExtractBase):
 
 
 ExtractZip().draw()
+
+import sys
+import os
+import streamlit as st
+
+st.markdown(os.curdir)
+st.markdown(os.path.abspath(os.curdir))
+st.markdown("\n".join(sys.path))
--- pages/02_gzip.py
+++ pages/02_gzip.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.join(os.path.dirname('__file__'), '..')
+)
+
 from typing import Any, BinaryIO, Callable
 
 from visualizer.base_extractor import ExtractBase
