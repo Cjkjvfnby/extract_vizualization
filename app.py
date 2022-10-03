5c9c0c9c57b3e7d4fd03ec90aee25f5c87da3470
Andrey <Cjkjvfnby@gmail.com>
2022-10-03 21:04:09 +0300

    Update intro table

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import streamlit as st
 st.title("Visualize archive extraction")
 
 
-zip_text = "[zip](https://en.wikipedia.org/wiki/ZIP_(file_format))"
-gzip_text = "[gzip](https://en.wikipedia.org/wiki/Gzip)"
+zip_text = "[wiki/ZIP](https://en.wikipedia.org/wiki/ZIP_(file_format))"
+gzip_text = "[wiki/Gzip](https://en.wikipedia.org/wiki/Gzip)"
 
 
 st.markdown(
     f"""
-| Archive | Number of files  | Read type  |  Visualization
-| :---    | ---              |---         |  ---
-| **{zip_text}**  | many | random access | [extract zip](/zip)
-| **{gzip_text}** | 1    | stream        | [extract gzip](/gzip)
+| Archive | Number of files  | Read type  |  Visualization | Documents link |
+| :---    | ---              |---         |  ---           | --- |
+| **zip**  | many | random access | [try it](/zip) | {zip_text} |
+| **gzip** | 1    | stream        | [try it](/gzip) | {gzip_text} |
 """
 )
