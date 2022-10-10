6b4b4e86b7ca266d421fb4c971e4787abf96d3a8
Andrey <Cjkjvfnby@gmail.com>
2022-10-10 21:45:39 +0300

    Fix pylint warnings

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import streamlit as st
 st.title("Visualize archive extraction")
 
 
-zip_text = "[wiki/ZIP](https://en.wikipedia.org/wiki/ZIP_(file_format))"
-gzip_text = "[wiki/Gzip](https://en.wikipedia.org/wiki/Gzip)"
+ZIP_TEXT = "[wiki/ZIP](https://en.wikipedia.org/wiki/ZIP_(file_format))"
+GZIP_TEXT = "[wiki/Gzip](https://en.wikipedia.org/wiki/Gzip)"
 
 
 st.markdown(
     f"""
 | Archive | Number of files  | Read type  |  Visualization | Documents link |
 | :---    | ---              |---         |  ---           | --- |
-| **zip**  | many | random access | [try it](/zip) | {zip_text} |
-| **gzip** | 1    | stream        | [try it](/gzip) | {gzip_text} |
+| **zip**  | many | random access | [try it](/zip) | {ZIP_TEXT} |
+| **gzip** | 1    | stream        | [try it](/gzip) | {GZIP_TEXT} |
 """
 )
