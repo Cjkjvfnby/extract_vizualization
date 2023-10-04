b4aa55199f895a49ce239579216854f81158c623
Andrey <Cjkjvfnby@gmail.com>
2023-10-04 20:23:06 +0200

    Simplify markdown

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import streamlit as st
 st.title("Visualize archive extraction")
 
 
-ZIP_TEXT = "[wiki/ZIP](https://en.wikipedia.org/wiki/ZIP_(file_format))"
-GZIP_TEXT = "[wiki/Gzip](https://en.wikipedia.org/wiki/Gzip)"
-
-
 st.markdown(
-    f"""
-| Archive | Number of files  | Read type  |  Visualization | Documents link |
-| :---    | ---              |---         |  ---           | --- |
-| **zip**  | many | random access | [try it](/zip) | {ZIP_TEXT} |
-| **gzip** | 1    | stream        | [try it](/gzip) | {GZIP_TEXT} |
+    """
+| Archive  | Number of files  | Read type     |  Visualization  | Documentation link    |
+| :---     | ---              |---            |  ---            | ---                   |
+| **zip**  | many             | random access | [try it](/zip)  | [wiki/ZIP][zip_doc]   |
+| **gzip** | 1                | stream        | [try it](/gzip) | [wiki/Gzip][gzip_doc] |
+
+
+[zip_doc]: https://en.wikipedia.org/wiki/ZIP_(file_format)
+[gzip_doc]: https://en.wikipedia.org/wiki/Gzip
+
 """,
 )
